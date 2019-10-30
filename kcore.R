935f7b5807c85fd67a14a52203fdfbce578fa6b0
Michael Ta <michael-ta@outlook.com>
2019-10-30 11:00:03 -0500

    weighted k-core on directed citation network

--- kcore.R
+++ kcore.R
@@ -1,11 +1,17 @@
 # k-core decomposition given graph G
 library(igraph)
 
+### Test Graph + unweighted k-core ###
+.f <- function() {
 
 G <- graph(c(1,2,  1,3,  2,3,  2,6,  3,4,  3,5,
              3,6,  3,7,  4,5,  4,7,  4,8,  5,7,
              5,8,  6,7,  6,9,  7,8,  7,9), 
           directed=F)
+
+for (i in 1:ecount(G)) {
+    G <- set_edge_attr(G, "weight", i, i)
+}
 
 ### unweighted k core decomposition ###
 k <- 1
@@ -27,14 +33,24 @@ while (vcount(Gcopy) > 0) {
     }
 }
 
-### weigthed k core decomposition according to Antonios Garas 2012 ###
-k <- 1
-Gcopy <- G
-for (i in 1:ecount(G)) {
-    G <- set_edge_attr(G, "weight", i, i)
-    Gcopy <- set_edge_attr(G, "weight", i, i)
 }
+
+### weigthed k core decomposition according to Antonios Garas 2012 ###
+data<-read.csv(file="./data/networkcitation.txt", sep="\t", header=F)
+colnames(data)<-c("from","to","time","weight")
+tokenGr<-graph_from_edgelist(as.matrix(data[,1:2]),directed=TRUE) 
+tokenGr<-graph_from_edgelist(as.matrix(data[,1:2]),directed=TRUE)%>%
+    set_vertex_attr("idx", value = as.character(seq(1,vcount(tokenGr))))
+E(tokenGr)$weight<-data$weight
+tokenGr<-delete_vertices((tokenGr), degree(tokenGr)==0)
+
+
+k <- 1
+
+Gcopy <- tokenGr
 nodes_to_delete <- c()
+cores <- c()
+
 while (vcount(Gcopy) > 0) {
     for (i in V(Gcopy)) {
         # get adjacent edges and sum the weights
@@ -42,20 +58,26 @@ while (vcount(Gcopy) > 0) {
         wi <- 0
         di <- length(adj_v)
         for (ai in adj_v) {
-            wi <- wi + edge_attr(Gcopy, "weight", get.edge.ids(Gcopy, c(i, ai)))
+            wi <- wi + edge_attr(Gcopy, "weight", get.edge.ids(Gcopy, c(ai, i)))
         }
         ki <- sqrt(di * wi)
-        if (ki < k) {
+        if (k > ki) {
             nodes_to_delete <- c(nodes_to_delete, i)
         }
     }
     if (length(nodes_to_delete) == 0) {
-        plot(Gcopy)
+        #if (vcount(Gcopy) < 200) {
+        #    plot(Gcopy)
+        #}
         k <- k + 1
     } else {
+        message("Number of nodes to be deleted: ", length(nodes_to_delete))
+        cores <- rbind(cores, cbind(vertex_attr(Gcopy)$idx[nodes_to_delete], k))
+
         Gcopy <- delete.vertices(Gcopy, nodes_to_delete)
         nodes_to_delete <- c()
     }
 }
 
+write.csv(cores, "results.csv")
 
